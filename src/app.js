940e87be4659b930ad7c70afc75abda18967c0f9
ヒエウ チャンチュン <N2207@nissin-mfg.co.jp>
2024-04-03 10:55:43 +0900

    add routes

--- src/app.js
+++ src/app.js
@@ -2,8 +2,7 @@ require('dotenv').config()
 const express = require('express'); // Import thư viện Express
 const morgan = require('morgan')
 const indexRouter = require('./routes/index') // Import den file web routes
-const indexSetting = require('./routes/setting')
-const indexContact = require('./routes')
+const settingRouter = require('./routes/setting')
 const app = express(); // Tạo một ứng dụng Express
 const path = require('path');
 const port = process.env.PORT || 3000; // Port
@@ -21,8 +20,7 @@ app.use(morgan('combined'));
 
 // Khai bao router
 app.use('/', indexRouter);
-app.use('/setting', indexSetting);
-
+app.use('/setting', settingRouter);
 
 app.listen(port, process.env.HOST_NAME, () => {
     console.log(`Example app listening on port ${port}`);
