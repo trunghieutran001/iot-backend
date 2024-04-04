a91cfbdf5e8ac003f0b0472738445583225285e2
ヒエウ チャンチュン <N2207@nissin-mfg.co.jp>
2024-04-04 11:08:24 +0900

    Thao tác với database

--- src/app.js
+++ src/app.js
@@ -3,8 +3,11 @@ const express = require('express'); // Import thư viện Express
 const morgan = require('morgan')
 const indexRouter = require('./routes/index') // Import den file web routes
 const settingRouter = require('./routes/setting')
+const mysql = require('mysql2');
+
 const app = express(); // Tạo một ứng dụng Express
 const path = require('path');
+const { table } = require('console');
 const port = 3000; // Port
 
 // config template engine
@@ -20,6 +23,37 @@ app.use(morgan('combined'));
 // Khai bao router
 app.use('/', indexRouter);
 app.use('/setting', settingRouter);
+
+// test connection
+// create the connection to database
+// Creating database connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3307,
+    user: 'root',
+    password: '123456',
+    database: 'appdb'
+});
+
+// Testing connection and performing a simple query
+connection.connect(function(err) {
+    if (err) {
+        console.error('Error connecting to database:', err);
+        return;
+    }
+
+    console.log('Connected to database');
+
+    connection.query('SELECT * FROM employees', function(err, results, fields) {
+        if (err) {
+            console.error('Error executing query:', err);
+            return;
+        }
+        console.log(">>> results=", results);
+        console.log(">>> fields=", fields);
+    });
+});
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
